aebd4d121a921fef4ab9c5a76f0adb95e5ce4614
kevingunawan123 <c14220047@john.petra.ac.id>
2023-06-24 21:42:52 +0700

    Tipe Material cek

--- src/main/java/com/example/bdjavafx/repositories/TipeMaterialRepository.java
+++ src/main/java/com/example/bdjavafx/repositories/TipeMaterialRepository.java
@@ -3,6 +3,7 @@ package com.example.bdjavafx.repositories;
 import com.example.bdjavafx.DBConnection;
 import com.example.bdjavafx.models.Produksi;
 import com.example.bdjavafx.models.TipeMaterial;
+import com.example.bdjavafx.models.TipeMesin;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -32,6 +33,15 @@ public class TipeMaterialRepository {
    public void createTipeMaterial(TipeMaterial tipeMaterial) throws SQLException {
       var conn = DBConnection.get();
       var stmt = conn.prepareStatement("INSERT INTO tipe_material VALUES (DEFAULT, ?, ?)" );
+      stmt.setString(1, tipeMaterial.getNamaTipe());
+      stmt.setString(2, tipeMaterial.getRecycleType());
+      stmt.execute();
+   }
+   public void updateTipeMaterial(TipeMaterial tipeMaterial) throws SQLException {
+      var conn = DBConnection.get();
+      var stmt = conn.prepareStatement("UPDATE tipeMaterial\n" +
+              "SET nama_tipe = ?, recycle_type = ?\n" +
+              "WHERE id_tipe = ?;" );
       stmt.setString(1, tipeMaterial.getNamaTipe());
       stmt.setString(2, tipeMaterial.getRecycleType());
       stmt.execute();
